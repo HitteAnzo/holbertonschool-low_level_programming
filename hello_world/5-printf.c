7cd200a111c9585d4abd7315b04a71dc5260d52a
root <root@LAPTOP-SI01MIVD>
2024-06-17 13:50:38 +0200

    5 fix

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
-
 /**
  * main - Print a text with printf
  *
  * Return: nothing
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
-	return (0)
-
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
 }
-
