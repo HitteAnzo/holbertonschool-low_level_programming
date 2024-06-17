f041e22a5b369e9953dea6db81d346b2bad99ca3
root <root@LAPTOP-SI01MIVD>
2024-06-17 13:21:31 +0200

    add descrition by using betty style to 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/**
+ * First program in C, prints exactly a text without using printf
+ *
+ * Return : Nothing
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
