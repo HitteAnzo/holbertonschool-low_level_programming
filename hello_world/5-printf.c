d513e51d106dd1b982b1aa7ccf4afb4b0fd41557
root <root@LAPTOP-SI01MIVD>
2024-06-17 13:40:30 +0200

    rename 5- wrong name

---
+++ hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <studio.h>
+
+/**
+ * main - Print a text with printf
+ *
+ * Return: nothing
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,")
+	return (0)
+
+}
+
