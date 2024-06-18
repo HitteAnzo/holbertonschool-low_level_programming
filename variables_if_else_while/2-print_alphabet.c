e844a6bf6aa509f6e8c4176d5b7fe30851a5022a
root <root@LAPTOP-SI01MIVD>
2024-06-18 13:39:23 +0200

    fix 2

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * main - Print lowercase alphabet.
  *
@@ -11,10 +10,11 @@ int main(void)
 	char alphabet;
 
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+
 	{
 		putchar(alphabet);
 	}
 	putchar('\n');
-	
+
 	return (0);
 }
