fd25abadb9cd938cf0e8e4e7b9bd5daa0ce1b1db
root <root@LAPTOP-SI01MIVD>
2024-06-18 15:23:06 +0200

    fix 6

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * main - Prints all digits of base 10
- * 
- * Return: Nothing.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int a
-		;
-	for (a = 0; a <= 9; a++)
-	{
-	putchar(48 + a);
-	}
-	putchar('\n');
+    int a;
 
-	return (0);
+    for (a = 0; a <= 9; a++)
+    {
+        putchar(48 + a);
+    }
+    putchar('\n');
+
+    return (0);
 }
