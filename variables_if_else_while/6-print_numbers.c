5617560c88af7302585b281c2f72509faefd56aa
root <root@LAPTOP-SI01MIVD>
2024-06-18 14:30:11 +0200

    test 6

---
+++ variables_if_else_while/6-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prints all digits of base 10
+ * 
+ * Return: Nothing.
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; i++)
+	{
+		putchar(048 + a);
+	}
+	putchar('\n');
+
+	return (0);
+}
