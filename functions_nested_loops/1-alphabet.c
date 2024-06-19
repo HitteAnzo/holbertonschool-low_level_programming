f43d9cfb1d5108651d9452f0cf6a1bf2d4c80e50
root <root@LAPTOP-SI01MIVD>
2024-06-19 21:06:41 +0200

    alphabet in 1-

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - print alphabet
+ *
+ * Description : print alphabet in lowercase
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+
+	putchar('\n');
+}
