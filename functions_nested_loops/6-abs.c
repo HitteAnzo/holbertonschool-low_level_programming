f2e724542e59b41ec0e721959f58812d3eb01674
root <root@LAPTOP-SI01MIVD>
2024-06-21 13:51:19 +0200

    abs val 6-

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - computes the abs val of an int
+ * @n: is the int to compute the absolute value of
+ * Return: Integer's absolute value
+ */
+int _abs(int n)
+{
+	if (n < 0) 
+	{
+		return (-n);
+	}
+	else
+	{
+	return (n);
+	}
+}
