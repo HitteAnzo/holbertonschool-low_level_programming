e5d4b193546410fae0c584f0e722b88d68b8e6d4
root <root@LAPTOP-SI01MIVD>
2024-06-24 17:10:42 +0200

    fix betty

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,23 +1,23 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- *
+ * print_line - Print x line for x numbers
+ * @n: number
  */
+
 void print_line(int n)
 {
 	if (n <= 0)
 	{
-	       	_putchar('\n');
+		_putchar('\n');
 	}
 	else
 	{
 		int i;
 
-        	for (i = 0; i < n; i++)
-        {
+		for (i = 1; i <= n; i++)
+		{
 			_putchar('_');
-       	}
-       			_putchar('\n');
+		}
+		_putchar('\n');
 	}
 }
