cb79a5f83a0514bd4e7111a42259fe269d6e1d8f
root <root@LAPTOP-SI01MIVD>
2024-06-24 13:06:35 +0200

    fix betty

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -3,7 +3,7 @@
 
 /**
  * _isdigit - checks if a character is a digit (0 through 9)
- * @c: the character to check 
+ * @c: the character to check
  * Return: 1 if c is a digit, 0 otherwise
  */
 int _isdigit(int c)
