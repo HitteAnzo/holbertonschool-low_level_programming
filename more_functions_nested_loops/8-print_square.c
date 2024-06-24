58c28f1883bd50365d758844c216881f74472d69
root <root@LAPTOP-SI01MIVD>
2024-06-24 17:47:03 +0200

    fix 8

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -5,8 +5,8 @@
  * print_square - Print a square
  * @size: size of the square
  */
-void print_square (int size)
-	
+void print_square(int size)
+
 {
 	if (size <= 0)
 	{
@@ -18,7 +18,7 @@ void print_square (int size)
 
 		for (i = 0; i < size; i++)
 		{
-			for (j =0; j < size; j++)
+			for (j = 0; j < size; j++)
 			{
 				_putchar('#');
 			}
