a43d2f56d3cd5080a38c04223e6afb47437e4f3f
root <root@LAPTOP-SI01MIVD>
2024-06-24 11:49:31 +0200

    checks for a digit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isdigit - checks if a character is a digit (0 through 9)
+ * @c: the character to check
+ * 
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+    return (c >= '0' && c <= '9');
+}
