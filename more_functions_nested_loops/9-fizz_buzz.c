b142c0240a78caf36b25fa89b3ea9ad7c5c602b0
root <root@LAPTOP-SI01MIVD>
2024-06-24 19:53:18 +0200

    fix 9-..

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,25 +12,26 @@ int main(void)
 	{
 		for (i = 1; i <= 100; i++)
 		{
-			if (i % 3 == 0)
+			if (i % 3 == 0 && i % 5 == 0)
 			{
-				printf(" Fizz ");
+				printf(" FizzBuzz ");
+			{
+			else if (i % 3 == 0)
+			{
+				printf("Fizz");
 			}
 			else if (i % 5 == 0)
 			{
-				printf(" Buzz ");
-			}
-			else if (i % 3 == 0 && i % 5 == 0)
-			{
-				printf("FizzBuzz");
-			}
-			else if (i == 1)
-			{
-				printf("%d", i);
+				printf("Buzz");
 			}
 			else 
 			{
 				printf("%d", i);
+			}
+
+			if (i < 100)
+			{
+				printf(" ");
 			}
 		}
 	}
