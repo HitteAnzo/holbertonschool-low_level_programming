67efed97ecd6dd22fb2b6944cd33a70b75564422
root <root@LAPTOP-SI01MIVD>
2024-06-24 11:20:55 +0200

    fix betty

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -4,10 +4,9 @@
 /**
  * _isupper - checks if a character is uppercase
  * @c: the character to check
- * 
  * Return: 1 if uppercase, 0 otherwise
  */
 int _isupper(int c)
-{
-    return (c >= 'A' && c <= 'Z');
-}
+	{
+	return (c >= 'A' && c <= 'Z');
+	}
