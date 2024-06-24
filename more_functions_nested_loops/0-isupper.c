7bec63326b544533b364fafe23d13bf131e072d9
root <root@LAPTOP-SI01MIVD>
2024-06-24 10:47:05 +0200

    Checks upper case in 0-

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks for uppercase character.
+ * @c: main.
+ * Return: 1 if uppercase 0 otherwise. 
+ */
+
+int _isupper(int c)
+
+	{
+		return (c >= 'A' && c <= 'Z');
+	}
