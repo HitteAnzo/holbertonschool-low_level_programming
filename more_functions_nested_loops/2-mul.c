f5797e1b8689e545df44c852a82ffb633c2a80a7
root <root@LAPTOP-SI01MIVD>
2024-06-24 13:11:08 +0200

    Fix 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiplie 2 numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: resultat
+ */
+int mul(int a, int b)
+
+{
+	int resultat;
+	resultat = a * b;
+	return (resultat);
+}
