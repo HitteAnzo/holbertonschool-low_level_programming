f6febb11cbb5090b4d72b8f30fff48fa0edbd12e
root <root@LAPTOP-SI01MIVD>
2024-06-24 18:44:14 +0200

    test 9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - print all numbers 1 to 100.
+ * 
+ * Description: Add Fizz, Buzz and FizzBuzz depenting of the multiple.
+ * Return: 0 on success.
+ */
+int main(void)
+{
+	int i;
+	{
+		for (i = 1; i <= 100; i++)
+		{
+			if (i % 3 == 0)
+			{
+				printf(" Fizz ");
+			}
+			else if (i % 5 == 0)
+			{
+				printf(" Buzz ");
+			}
+			else if (i % 3 == 0 && i % 5 == 0)
+			{
+				printf("FizzBuzz");
+			}
+			else if (i == 1)
+			{
+				printf("%d", i);
+			}
+			else 
+			{
+				printf("%d", i);
+			}
+		}
+	}
+	return (0);
+}
