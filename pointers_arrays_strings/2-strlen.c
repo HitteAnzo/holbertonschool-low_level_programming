6230bb2fb28ff68203e3fb082c8eca0fb66466df
root <root@LAPTOP-SI01MIVD>
2024-06-25 18:14:19 +0200

    fix {} before return

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen _ return the length of string.
+ * @s: string
+ * Return: lenght
+ */
+
+int _strlen(char *s)
+{
+	int lenght;
+
+	for (lenght = 0; s[lenght] != 0; lenght++)
+	{
+	}
+	return (lenght);
+}
