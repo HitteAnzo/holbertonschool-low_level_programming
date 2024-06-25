90883540a2160d4dc66dfdc76c916aeb71eb4917
root <root@LAPTOP-SI01MIVD>
2024-06-25 18:37:04 +0200

    try 3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - print string
+ * @str: string
+ */
+
+void _puts(char *str)
+{
+	int i; 
+	for (i=0; str[i] != 0; i++)
+	{
+		_putchar(str[i);
+	}
+	_putchar('\n')
+}
