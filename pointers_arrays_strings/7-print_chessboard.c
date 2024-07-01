8bd1b175f54ae0fb7e6fb9fd105201c50fe8413c
root <root@LAPTOP-SI01MIVD>
2024-07-01 13:23:17 +0200

    fix betty

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -4,9 +4,6 @@
 /*
  * print_chessboard - Prints the chessboard represented by a 2D array of characters.
  * @a: Pointer to the 2D array of characters representing the chessboard.
- *
- * This function iterates through each row of the chessboard array and prints
- * each character to stdout using putchar. Each row is printed on a new line.
  */
 void print_chessboard(char (*a)[8])
 {
