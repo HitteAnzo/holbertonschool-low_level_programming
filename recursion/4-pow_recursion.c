aee5394955456fb3746922bd2222921f941ab4e7
root <root@LAPTOP-SI01MIVD>
2024-07-02 15:40:11 +0200

    fix betty

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -10,9 +10,9 @@
 
 int _pow_recursion(int x, int y)
 {
-	if (y < 0) 
+	if (y < 0)
 		return (-1);
 	if (y == 0)
 		return (1);
-	return x * _pow_recursion(x, y - 1);
+	return (x * _pow_recursion(x, y - 1));
 }
