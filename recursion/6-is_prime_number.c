046009c75552d78069aaa8a2cd5cc1175c32af7b
root <root@LAPTOP-SI01MIVD>
2024-07-03 11:47:03 +0200

    fix 6

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *
+ */
+int is_prime( int n, int c)
+{
+	if (n == c)
+		return (1);
+
+	else if (n % c == 0)
+		return (0);
+
+	else if (n * n == c)
+		return (n);
+
+	return (is_prime(n, c + 1));
+}
+
+/**
+ *
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+
+	return (is_prime(n, 2));
+}
