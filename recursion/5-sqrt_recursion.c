d8877d3faac9c8f847e8fd1fc55ce142531e6cf4
root <root@LAPTOP-SI01MIVD>
2024-07-03 09:53:41 +0200

    5- fix

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *
+ */
+int power_operation(int n, int c)
+{
+	if (c % (n / c) == 0)
+	{
+		if (c * (n / c) == n)
+			return (c);
+		else
+			return (-1);
+	}
+	return (0 + power_operation(n, c + 1));
+
+
+/**
+ *
+ */ 
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n = 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return (power_operation(n, 2));
+}
