f2dadacbd6a40a7ffc00879edadfd64242b268e0
HitteAnzo <hitteanzo@gmail.com>
2024-07-15 14:09:51 +0200

    update 3

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,14 +1,16 @@
-#include"3-calc.h"
-#include<stdlib.h>
-#include<stdio.h>
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * get_op_func - function to look.
- * @s: operator pass as argument. MLP.
- * Return: pointer to the correspondant given operator.
+ * get_op_func -  operation function. MLP.
+ * @s: operator (+, -, *, /, %).
+ * Return: function pointer.
  */
-int (*get_op_func(char *s))(int a, int b)
+int (*get_op_func(char *s))(int, int)
 {
+	int i;
+
 	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
@@ -16,14 +18,15 @@ int (*get_op_func(char *s))(int a, int b)
 		{"/", op_div},
 		{"%", op_mod},
 		{NULL, NULL}
-	}
+	};
 
-	int i = 0;
-
+	i = 0;
 	while (ops[i].op != NULL)
 	{
-		if (*s == *ops[i].op)
+		if (*ops[i].op == *s && s[1] == '\0')
+		{
 			return (ops[i].f);
+		}
 		i++;
 	}
 	return (NULL);
