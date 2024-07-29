67fb879f07e10cfde9acaf05ce6369725e46e221
HitteAnzo <hitteanzo@gmail.com>
2024-07-29 11:56:03 +0200

    fix 0-

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -8,7 +8,7 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
+	size_t node = 0;
 
 	while (h != NULL)
 	{
@@ -16,9 +16,10 @@ size_t print_list(const list_t *h)
 			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-		nodes++;
+		node++;
+		h = h->next;
 
 	}
 
-	return (nodes);
+	return (node);
 }
