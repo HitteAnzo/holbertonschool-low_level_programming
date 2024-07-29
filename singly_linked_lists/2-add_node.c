da676c2e567631bc6a923e7f1ff790116b52a0a4
HitteAnzo <hitteanzo@gmail.com>
2024-07-29 14:37:43 +0200

    fix 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -6,31 +6,31 @@
  * add_node - adds a new node at the beginning of list_t list. MLP
  * @head: a pointer to the head of list_t list.
  * @str: the string to be added to list_t list.
- * Return: NULL if fail, the address of the new node (newn) if success.
+ * Return: NULL if fail, the new node (newn) if success.
  */
 list_t *add_node(list_t **head, const char *str)
 {
 	char *dup;
 	int len;
-	list_t *new_node;
+	list_t *newn;
 
-	new_node = malloc(sizeof(list_t));
-	if (!new_node)
+	newn = malloc(sizeof(list_t));
+	if (!newn)
 		return (NULL);
 
 	dup = strdup(str);
 	if (!dup)
 	{
-		free(new_node);
+		free(newn);
 		return (NULL);
 	}
 
-	len = strlen(new_node);
+	len = strlen(str);
 
-	new_node->str = dup;
-	new_node->len = len;
-	new_node->next = *head;
-	*head = new_node;
+	newn->str = dup;
+	newn->len = len;
+	newn->next = *head;
+	*head = newn;
 
-	return (new_node);
+	return (newn);
 }
