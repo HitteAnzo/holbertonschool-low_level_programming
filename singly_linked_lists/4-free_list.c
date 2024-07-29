c7bf2cc8063ae372e7a6eaeae7f0cbd950b3dfc8
HitteAnzo <hitteanzo@gmail.com>
2024-07-29 17:17:19 +0200

    update 4.

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - free list_t
+ * @head: pointer to the beginning of the list
+ */
+void free_list(list_t *head)
+{
+	while (head != NULL)
+	{
+		list_t *temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
