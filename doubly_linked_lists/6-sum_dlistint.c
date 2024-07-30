2ed76ce379e0da01b4b95371198328f4999c8e9c
HitteAnzo <hitteanzo@gmail.com>
2024-07-30 04:55:41 +0200

    6-..

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - sum all data (n) of a list.
+ * @head: beginning of the list.
+ * Return: 0 if empty.
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head != NULL)
+	{
+		sum += head->n
+		head = head->next
+	}
+	return (sum);
+}
