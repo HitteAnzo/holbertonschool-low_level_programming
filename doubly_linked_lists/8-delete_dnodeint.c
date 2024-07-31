c768a2d03af8bbf3b447544713707648c80587f4
HitteAnzo <hitteanzo@gmail.com>
2024-07-31 10:20:56 +0200

    Update 8-delete_dnodeint.c

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -38,7 +38,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
         if (temp->next)
         temp->next->prev = temp ->prev;
     }
-    
-    free(temp)
+
+    free(temp);
     return (1);
 }
