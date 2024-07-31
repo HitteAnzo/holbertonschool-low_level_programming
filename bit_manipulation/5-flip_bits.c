14b1cdb47bd8439d0aaeed4b4cd2c5ce5f66a254
HitteAnzo <hitteanzo@gmail.com>
2024-07-31 16:30:30 +0200

    create 5-..

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * flip_bits - counts the number of bits needed to flip to convert one number to another
+ * @n: the first number
+ * @m: the second number
+ * Return: the number of bits that need to be flipped
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int xor = n ^ m;
+	unsigned int count = 0;
+
+	while (xor)
+	{
+		count += xor & 1;
+		xor >>= 1;
+	}
+
+	return count;
+}
