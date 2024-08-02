bb8007d8bd55daf361ada3592ed80e2dc8a9a965
HitteAnzo <hitteanzo@gmail.com>
2024-08-02 12:02:32 +0200

    Create 0-read_textfile.c

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+textfile - reads a text file and prints the letters.
+* @filename: file's name.
+* @letters : number of letters read & printed.
+* Return : numbers of letters printed, 0 if failure.
+*/
+ssize_t read_textfile(const char* filename, size_t letters)
+{
+    int fd;
+    ssize_t n_read, n_written;
+    char* buffer;
+
+    if (filename == NULL)
+        return (0);
+
+    buffer = malloc(sizeof(char) * letters);
+    if (buffer == NULL)
+        return (0);
+
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+    {
+        free(buffer);
+        return (0);
+    }
+
+    n_read = read(fd, buffer, letters);
+    if (n_read == -1)
+    {
+        free(buffer);
+        close(fd);
+        return (0);
+    }
+
+    n_written = write(STDOUT_FILENO, buffer, n_read);
+    if (n_written == -1 || n_written != n_read)
+    {
+        free(buffer);
+        close(fd);
+        return (0);
+    }
+
+    free(buffer);
+    close(fd);
+
+    return (n_written);
+}
