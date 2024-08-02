649b25bb5d1d62e360f0daa11600a11f6a94d5ac
HitteAnzo <hitteanzo@gmail.com>
2024-08-02 15:27:18 +0200

    Update 3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,45 +1,39 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include "main.h"
+#include <stdio.h>
+
 /**
- * handle_error - Handles errors by closing file descriptors and printing a message
- * @fd_s: Source file descriptor
- * @fd_d: Destination file descriptor
- * @error_code: Error code to exit with
- * @message: Error message to print
- *
- * Description: Closes file descriptors if they are open and prints an error
- * message to the standard error. Then, exits with the specified error code.
+ * errors - handle errors
+ * @fd_s: source file
+ * @fd_d: destination file
+ * @argv: array of arguments
+ * Return: void
  */
-void handle_error(int fd_s, int fd_d, int error_code, const char *message)
+void errors(int fd_s, int fd_d, char *argv[])
 {
-    if (fd_s != -1)
+    if (fd_s == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+        exit(98);
+    }
+
+    if (fd_d == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
         close(fd_s);
-
-    if (fd_d != -1)
-        close(fd_d);
-
-    dprintf(STDERR_FILENO, "%s\n", message);
-    exit(error_code);
+        exit(99);
+    }
 }
 
 /**
- * main - Copies the content of one file to another
- * @argc: Number of arguments
- * @argv: Array of arguments
- *
- * Description: Copies the content from the file specified by argv[1] to the
- * file specified by argv[2]. Exits with specific codes and messages in case
- * of errors.
- *
- * Return: 0 on success, or exits with a specific error code
+ * main - program that copies the content of one file to another
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 (Always)
  */
 int main(int argc, char *argv[])
 {
-    const char *f_from;
-    const char *f_to;
+    const char *f_from = argv[1];
+    const char *f_to = argv[2];
     int fd_s, fd_d;
     char buf[1024];
     ssize_t nrd, nwr;
@@ -50,33 +44,28 @@ int main(int argc, char *argv[])
         exit(97);
     }
 
-    f_from = argv[1];
-    f_to = argv[2];
-
     fd_s = open(f_from, O_RDONLY);
-    if (fd_s == -1)
-    {
-        handle_error(-1, -1, 98, "Error: Can't read from file");
-    }
-
     fd_d = open(f_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-    if (fd_d == -1)
-    {
-        handle_error(fd_s, -1, 99, "Error: Can't write to file");
-    }
+    errors(fd_s, fd_d, argv);
 
     while ((nrd = read(fd_s, buf, sizeof(buf))) > 0)
     {
         nwr = write(fd_d, buf, nrd);
         if (nwr == -1)
         {
-            handle_error(fd_s, fd_d, 99, "Error: Can't write to file");
+            dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+            close(fd_s);
+            close(fd_d);
+            exit(99);
         }
     }
 
     if (nrd == -1)
     {
-        handle_error(fd_s, fd_d, 98, "Error: Can't read from file");
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+        close(fd_s);
+        close(fd_d);
+        exit(98);
     }
 
     if (close(fd_s) == -1)
