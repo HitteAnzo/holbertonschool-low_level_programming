bfa339def9cd82a04cfc1fab4124a34465aeb12a
HitteAnzo <hitteanzo@gmail.com>
2024-08-02 15:19:19 +0200

    Update 3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,96 +1,84 @@
+#include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
 
 /**
- * handle_error - Handles errors by closing file descriptors and printing a message
- * @fd_s: Source file descriptor
- * @fd_d: Destination file descriptor
- * @argv: Array of arguments
- * @error_code: Error code to exit with
- * @message: Error message to print
- *
- * Description: Closes file descriptors if they are open and prints an error
- * message to the standard error. Then, exits with the specified error code.
+ * errors - handle errors
+ * @fd_s: source file
+ * @fd_d: destination file
+ * @argv: array of arguments
+ * Return: void
  */
-void handle_error(int fd_s, int fd_d, char *argv[], int error_code, const char *message)
+void errors(int fd_s, int fd_d, char* argv[])
 {
-	(void)argv;
+    if (fd_s == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+        exit(98);
+    }
 
-	if (fd_s != -1)
-		close(fd_s);
-
-	if (fd_d != -1)
-		close(fd_d);
-
-	dprintf(STDERR_FILENO, "%s\n", message);
-	exit(error_code);
+    if (fd_d == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+        close(fd_s);
+        exit(99);
+    }
 }
 
 /**
- * main - Copies the content of one file to another
- * @argc: Number of arguments
- * @argv: Array of arguments
- *
- * Description: Copies the content from the file specified by argv[1] to the
- * file specified by argv[2]. Exits with specific codes and messages in case
- * of errors.
- *
- * Return: 0 on success, or exits with a specific error code
+ * main - program that copies the content of one file to another
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 (Always)
  */
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-	const char *f_from;
-	const char *f_to;
-	int fd_s, fd_d;
-	char buf[1024];
-	ssize_t nrd, nwr;
+    const char* f_from = argv[1];
+    const char* f_to = argv[2];
+    int fd_s, fd_d;
+    char buf[1024];
+    ssize_t nrd, nwr;
 
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+    if (argc != 3)
+    {
+        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+        exit(97);
+    }
 
-	f_from = argv[1];
-	f_to = argv[2];
+    fd_s = open(f_from, O_RDONLY);
+    fd_d = open(f_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+    errors(fd_s, fd_d, argv);
 
-	fd_s = open(f_from, O_RDONLY);
-	if (fd_s == -1)
-	{
-		handle_error(-1, -1, argv, 98, "Error: Can't read from file");
-	}
+    while ((nrd = read(fd_s, buf, sizeof(buf))) > 0)
+    {
+        nwr = write(fd_d, buf, nrd);
+        if (nwr == -1)
+        {
+            dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+            close(fd_s);
+            close(fd_d);
+            exit(99);
+        }
+    }
 
-	fd_d = open(f_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_d == -1)
-	{
-	handle_error(fd_s, -1, argv, 99, "Error: Can't write to file");
-	}
+    if (nrd == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+        close(fd_s);
+        close(fd_d);
+        exit(98);
+    }
 
-	while ((nrd = read(fd_s, buf, sizeof(buf))) > 0)
-	{
-		nwr = write(fd_d, buf, nrd);
-		if (nwr == -1)
-		{
-			handle_error(fd_s, fd_d, argv, 99, "Error: Can't write to file");
- 		}
-	}
+    if (close(fd_s) == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_s);
+        exit(100);
+    }
 
-	if (nrd == -1)
-	{
-		handle_error(fd_s, fd_d, argv, 98, "Error: Can't read from file");
-	}
+    if (close(fd_d) == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_d);
+        exit(100);
+    }
 
-	if (close(fd_s) == -1)
-	{
-		handle_error(-1, fd_d, argv, 100, "Error: Can't close fd");
-	}
-
-	if (close(fd_d) == -1)
-	{
-		handle_error(-1, fd_d, argv, 100, "Error: Can't close fd");
-	}
-
-	return (0);
+    return (0);
 }
