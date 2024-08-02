0f08359e95bc6a727060cb5b13b68ba30ea6425e
HitteAnzo <hitteanzo@gmail.com>
2024-08-02 12:29:25 +0200

    fix all betty

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -9,28 +9,28 @@
  */
 int create_file(const char* filename, char* text_content)
 {
-    int fd, nletters, rwr;
+	int fd, nletters, rwr;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-    if (fd == -1)
-        return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
 
-    if (text_content != NULL)
-    {
-        for (nletters = 0; text_content[nletters]; nletters++)
-            ;
+	if (text_content != NULL)
+	{
+		for (nletters = 0; text_content[nletters]; nletters++)
+			;
 
-        rwr = write(fd, text_content, nletters);
-        if (rwr == -1)
-        {
-            close(fd);
-            return (-1);
-        }
-    }
+		rwr = write(fd, text_content, nletters);
+		if (rwr == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
 
-    close(fd);
-    return (1);
+	close(fd);
+	return (1);
 }
