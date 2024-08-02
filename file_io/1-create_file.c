b41f5d5890825d3e57f7aa4a21398f6c339a199c
Hitte Anzo <166728678+HitteAnzo@users.noreply.github.com>
2024-08-02 12:39:56 +0200

    Betty 1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -7,7 +7,7 @@
  *
  * Return: 1 on success, -1 on failure
  */
-int create_file(const char* filename, char* text_content)
+int create_file(const char *filename, char *text_content)
 {
 	int fd, nletters, rwr;
 
