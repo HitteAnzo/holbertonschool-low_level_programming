dd38cdc74a95c1b521f8e79e018e1d02f4359125
Hitte Anzo <166728678+HitteAnzo@users.noreply.github.com>
2024-08-02 12:41:30 +0200

    Betty 2-append_text_to_file.c

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -7,7 +7,7 @@
  *
  * Return: 1 on success, -1 on failure.
  */
-int append_text_to_file(const char* filename, char* text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, nletters, rwr;
 
